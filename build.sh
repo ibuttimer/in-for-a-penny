0fd9f37b62258e2fcd71f4311362c17b2300e203
Ian Buttimer <ibuttimer@yahoo.com>
2023-04-03 11:12:00 +0100

    maint: Add python header files for psycopg2 build

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 # Render.com buildscript
 set -o errexit
+
+sudo apt-get install python-dev
+sudo apt-get install python3-dev
+
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py makemigrations && python manage.py migrate
